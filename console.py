2d260586c65e590335f443ef30b890f5202d9ba4
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-03-10 00:37:27 +0300

    Addinf functionality to the console

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import cmd
-
+from models.base_model import BaseModel
 
 """"this module holds the console program"""
 
@@ -15,6 +15,28 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """"Exits the Console"""
         return True
+
+    def do_create(self, arg):
+        """
+        creates a new instance of basemodel, saves
+        it json file and prints the id.
+        """
+        if arg == "BaseModel":
+            obj = BaseModel()
+            obj.save()
+            print("{}".format(obj.id))
+        elif not arg:
+            print("{}".format("** class name missing **"))
+        else:
+            print("{}".format("** class doesn't exist **"))
+
+    def do_show(self, *arg):
+        """
+        Prints the string representation of an instance based
+        on the class name and id.
+        """
+        print(*arg)
+
 
 
 if __name__ == '__main__':

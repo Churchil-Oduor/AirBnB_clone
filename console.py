1b54d021fd5d55537dd0c34d185eff59e2e9ee1c
Churchil-Oduor <churchilokechoduor@gmail.com>
2024-03-10 21:06:55 +0300

    Will come back to the branch

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import cmd
 from models.base_model import BaseModel
-
+from models.engine.file_storage import FileStorage
 """"this module holds the console program"""
 
 
@@ -35,7 +35,25 @@ class HBNBCommand(cmd.Cmd):
         Prints the string representation of an instance based
         on the class name and id.
         """
-        print(*arg)
+        storage = FileStorage()
+        objects = storage.__objects
+        classes = ["BaseModel"]
+        if arg:
+            if len(arg) == 2:
+                if arg[0] == "BaseModel":
+                    try:
+                        obj_key = "BaseModel."+str(arg[1])
+                        obj = objects[obj_key]
+                        print(obj)
+                    except:
+                        print("{}".format("** no instance found **"))
+                else:
+                    print("{}".format("** class doesn't exist **"))
+            else:
+                pass
+
+        else:
+            print("{}".format("** class name missing **"))
 
 
 
